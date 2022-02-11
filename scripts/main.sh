3942f4997c479050d08b010385171bf479257358
vicalloy <zbirder@gmail.com>
2022-02-11 11:25:33 +0800

    update script

--- scripts/main.sh
+++ scripts/main.sh
@@ -34,13 +34,35 @@ function create_outline_env_file {
     fn=env.outline
     env_file=../$fn
     cp ./templates/$fn $env_file
+
+    SECRET_KEY=`openssl rand -hex 32`
+    UTILS_SECRET=`openssl rand -hex 32`
+
+    env_replace URL $URL $env_file
+    env_replace SECRET_KEY $SECRET_KEY env.outline
+    env_replace UTILS_SECRET $UTILS_SECRET env.outline
     env_replace DEFAULT_LANGUAGE $DEFAULT_LANGUAGE $env_file
+
+    env_delete DATABASE_URL
+    env_delete DATABASE_URL_TEST
+    env_delete REDIS_URL
+    env_delete AWS_S3_UPLOAD_BUCKET_NAME
+
     env_replace AWS_ACCESS_KEY_ID $MINIO_ACCESS_KEY $env_file
     env_replace AWS_SECRET_ACCESS_KEY $MINIO_SECRET_KEY $env_file
-
     env_replace AWS_S3_UPLOAD_BUCKET_URL $URL $env_file
-    env_replace URL $URL $env_file
-    AWS_S3_UPLOAD_BUCKET_NAME URL outline-bucket $env_file
+}
+
+function create_oidc_env_file {
+    fn=env.oidc
+    env_file=../$fn
+    cp ./templates/$fn $env_file
+
+    env_replace OIDC_CLIENT_ID $OIDC_CLIENT_ID $env_file
+    env_replace OIDC_CLIENT_SECRET $OIDC_CLIENT_SECRET $env_file
+    env_replace OIDC_AUTH_URI $OIDC_AUTH_URI $env_file
+    env_replace OIDC_TOKEN_URI $OIDC_TOKEN_URI $env_file
+    env_replace OIDC_USERINFO_URI $OIDC_USERINFO_URI $env_file
 }
 
 function create_env_files {
@@ -48,6 +70,7 @@ function create_env_files {
     create_nginx_env_file
     create_minio_env_file
     create_outline_env_file
+    create_oidc_env_file
 }
 
 function generate {

138e141e768e47a53fe5c996cda20d3dfc9e41c2
vicalloy <zbirder@gmail.com>
2022-02-17 12:47:13 +0800

    add comment in scripts/config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,5 @@
+# The url used to vist the web site.
+# The url should be accessible in host machine and docker container(Can't be localhost or 127.0.0.1).
 URL=http://192.168.x.x:8888
 # The default interface language. See translate.getoutline.com for a list of
 # available language codes and their rough percentage translated.
@@ -9,9 +11,13 @@ TIME_ZONE=UTC
 FORCE_HTTPS=false
 
 # Nginx
+# The nginx bind ip and port.
+# If you use ip address to access outline, this ip and port should be same as the URL.
+# If this server behind a proxy(nginx), you can bind to `127.0.0.1` .
 HTTP_IP=192.168.x.x
 HTTP_PORT_IP=8888
 
 # Docker
+# If you server behind a proxy(nginx), and the proxy created by docker. You can use the proxy's network. Set the `NETWORKS` to proxy's network name, and set `NETWORKS_EXTERNAL` to `true` .
 NETWORKS=outlinewiki
 NETWORKS_EXTERNAL=false
